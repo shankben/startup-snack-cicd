0f42debb82eff5498852263ae4b1c9c6693ca16a
Ben Shank <benshank@amazon.com>
2020-12-09 08:30:43 -0600

    wip

--- lib/stacks/api.ts
+++ lib/stacks/api.ts
@@ -24,7 +24,7 @@ export default class RestApiStack extends Stack {
   public readonly api;
   public readonly authorizer;
 
-  private readonly assetPath = path.join(__dirname, "..", "..", "..", "src",
+  private readonly assetPath = path.join(__dirname, "..", "..", "src",
     "lambda");
 
   private integrationLambda(name: string, ...assetPath: string[]) {
