9c817a8f6e23e51421f9b173507949e15a0f0ad1
Ben Shank <benshank@amazon.com>
2020-12-09 08:22:21 -0600

    wip

--- bin/app.ts
+++ bin/app.ts
@@ -1,18 +1,27 @@
-import { execSync } from "child_process";
+import fs from "fs";
+import path from "path";
 import "source-map-support/register";
 import { App } from "@aws-cdk/core";
 import PipelineStack from "../lib/stacks/pipeline";
 import { ensureSecrets } from "../lib/utils";
 
+const pjp = path.join(__dirname, "..", "..", "package.json");
+const packageJson = JSON.parse(fs.readFileSync(pjp).toString());
+const {
+  gitHubBranch,
+  gitHubOwner,
+  gitHubRepository,
+  gitHubTokenSecretId
+} = packageJson.config;
+
 async function main() {
   await ensureSecrets();
-  const gitHubBranch = execSync("git branch --show-current").toString().trim();
   const app = new App();
   new PipelineStack(app, `CDKSnackCICD-${gitHubBranch}`, {
     gitHubBranch,
-    gitHubOwner: "shankben",
-    gitHubRepository: "cdk-snack-cicd",
-    gitHubTokenSecretId: "/CDKSnackCICD/GitHubAccessToken"
+    gitHubOwner,
+    gitHubRepository,
+    gitHubTokenSecretId
   });
 }
 
