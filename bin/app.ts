398884f51c274f0dba020ecf51003e4f546fd2da
Ben Shank <benshank@amazon.com>
2020-12-09 08:12:24 -0600

    wip

--- bin/app.ts
+++ bin/app.ts
@@ -4,6 +4,8 @@ import { App } from "@aws-cdk/core";
 import PipelineStack from "../lib/stacks/pipeline";
 import { ensureSecrets } from "../lib/utils";
 
+console.log(process.env);
+
 async function main() {
   await ensureSecrets();
   const gitHubBranch = execSync("git branch --show-current").toString().trim();
